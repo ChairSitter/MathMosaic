ce6c8cb7ba0368b1d5262d253f5bc34a7c28b9c3
Bryan Schneller <bryan.schneller@gmail.com>
2025-01-16 13:56:49 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const container = document.getElementById('container');
+const containerSize = 11;
+
+const stateArray = [];
+
+//creates the game grid
+
+for(let column = 0; column < containerSize; column++){
+    for(let row = 0; row < containerSize; row++){
+        let cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.dataset.column = column;
+        cell.dataset.row = row;
+        if(column === 5 && row === 5){
+            cell.classList.add('center');
+        }
+        stateArray.push(cell);
+        container.appendChild(cell);
+    }
+}
+
+document.addEventListener('click', () => console.log(stateArray));
+
+//run the game
+
