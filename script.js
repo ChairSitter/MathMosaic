3a542fb2440fc6ff302d7b354927c10c41a3e0e0
Bryan Schneller <bryan.schneller@gmail.com>
2025-01-16 14:43:12 -0500

    Create 2d grid and assign textContent to surrounding cells with numbers 1-8

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const container = document.getElementById('container');
 const containerSize = 11;
 
-const stateArray = [];
+//change to array of arrays
+const cellArray = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
 
 //creates the game grid
 
@@ -14,22 +27,35 @@ for(let column = 0; column < containerSize; column++){
         if(column === 5 && row === 5){
             cell.classList.add('center');
         }
-        stateArray.push(cell);
+        cellArray[column][row] = cell;
         container.appendChild(cell);
     }
 }
 
-// console.log(stateArray);
+console.log(cellArray);
 
-//run the game
+//game variables
 const mosaicArray = [];
 const curCell = [5, 5];
+const curVal = 5;
 
 const curTar = 10; //from mosaicArray
 
-for(let i = 0; i < stateArray.length; i++){
-    if(stateArray[i].dataset.column == curCell[0] && stateArray[i].dataset.row == curCell[1]){
-        console.log(stateArray[i], 'logging current cell');
+//access specific cell using curCell variable
+for(let column = 0; column < containerSize; column++){
+    for(let row = 0; row < containerSize; row++){
+        if(cellArray[column][row].dataset.column == curCell[0] && cellArray[column][row].dataset.row == curCell[1]){
+            console.log(cellArray[column][row], 'logging current cell');
+            cellArray[column][row].textContent = curVal;
+            cellArray[column-1][row+1].textContent = 1;
+            cellArray[column][row+1].textContent = 2;
+            cellArray[column+1][row+1].textContent = 3;
+            cellArray[column+1][row].textContent = 4;
+            cellArray[column+1][row-1].textContent = 5;
+            cellArray[column][row-1].textContent = 6;
+            cellArray[column-1][row-1].textContent = 7;
+            cellArray[column-1][row].textContent = 8;
+        }
     }
 }
 
