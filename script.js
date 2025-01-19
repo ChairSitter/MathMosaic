22d1f38f438118e7120fb36b132777adfa92c872
Bryan Schneller <bryan.schneller@gmail.com>
2025-01-19 15:18:13 -0500

    Begin building game logic

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ class Cell {
     }
 }
 
-//creates the game grid
+//creates the game grid and cells
 for(let row = 0; row < containerSize; row++){
     for(let column = 0; column < containerSize; column++){
         let cell = new Cell(row, column, document.createElement('div'));
@@ -76,45 +76,36 @@ for(let row = 0; row < containerSize; row++){
     }
 }
 
-console.log(cellArray);
-
-//game variables
-const mosaicArray = [];
-const curCell = [5, 5];
-const curVal = 5;
-
-const curTar = 10; //from mosaicArray
-
 //assign relationships between cells
 for(let row = 0; row < containerSize; row++){
     for(let column = 0; column < containerSize; column++){
         let thisCell = cellArray[row][column];
         if(row > 0 && column < 10) thisCell.topRight = cellArray[row-1][column+1];
-        if(column < 10) thisCell.topRight = cellArray[row][column+1];
-        if(row < 10 && column < 10) thisCell.topRight = cellArray[row+1][column+1];
-        if(row < 10) thisCell.topRight = cellArray[row+1][column];
-        if(row < 10 && column > 0) thisCell.topRight = cellArray[row+1][column-1];
-        if(column > 0) thisCell.topRight = cellArray[row][column-1];
-        if(row > 0 && column > 0) thisCell.topRight = cellArray[row-1][column-1];
-        if(row > 0)thisCell.topRight = cellArray[row-1][column];
+        if(column < 10) thisCell.right = cellArray[row][column+1];
+        if(row < 10 && column < 10) thisCell.botRight = cellArray[row+1][column+1];
+        if(row < 10) thisCell.bot = cellArray[row+1][column];
+        if(row < 10 && column > 0) thisCell.botLeft = cellArray[row+1][column-1];
+        if(column > 0) thisCell.left = cellArray[row][column-1];
+        if(row > 0 && column > 0) thisCell.topLeft = cellArray[row-1][column-1];
+        if(row > 0)thisCell.top = cellArray[row-1][column];
     }
 }
+
+//game variables
+const mosaicArray = [13, 9, 10, 11];
+const curCell = [5, 5];
+let curVal = 5;
+
+const curTar = 10; //from mosaicArray
+
+
 
 //access specific cell using curCell variable
 for(let row = 0; row < containerSize; row++){
     for(let column = 0; column < containerSize; column++){
-        if(cellArray[row][column].div.dataset.row == curCell[0] && cellArray[row][column].getDiv().dataset.column == curCell[1]){
+        if(cellArray[row][column].row == curCell[0] && cellArray[row][column].column == curCell[1]){
             console.log(cellArray[row][column], 'logging current cell');
-            cellArray[row][column].div.textContent = curVal; //current
-
-            cellArray[row-1][column+1].div.textContent = 1; //topRight
-            cellArray[row][column+1].div.textContent = 2; //right
-            cellArray[row+1][column+1].div.textContent = 3; //botRight
-            cellArray[row+1][column].div.textContent = 4; //bot
-            cellArray[row+1][column-1].div.textContent = 5; //botLeft
-            cellArray[row][column-1].div.textContent = 6; //left
-            cellArray[row-1][column-1].div.textContent = 7; //topLeft
-            cellArray[row-1][column].div.textContent = 8; //top
+            
         }
     }
 }
